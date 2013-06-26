cc4c24668ddc334f40be109d28440d9b0d944cb0
Krishna K. Sharma <krishna.sharma@ymedialabs.com>
2013-06-26 21:47:05 +0530

    Initial commit:Addition of carousel

---
+++ src/k/k/sharma/corouselpagerkk/Util.java
@@ -0,0 +1,12 @@
+package k.k.sharma.corouselpagerkk;
+
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class Util {
+
+	public static int dpToPx(Resources res, int dp) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics());
+	}
+
+}
